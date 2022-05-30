f8ec224834654b2c1d03ff5081302bd685aa05c3
igor05k <igormedfer@gmail.com>
2022-05-30 18:27:44 -0300

    Initial Commit

---
+++ ToDo/ToDoViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Igor Fernandes on 30/05/22.
+//
+
+import UIKit
+
+class ToDoViewController: UITableViewController {
+    
+    let items = ["item 1", "item 2", "item 3"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath)
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
